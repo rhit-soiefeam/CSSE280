95312afafdb23802faca4fc19efea4ad25cfa5ff
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2023-09-11 14:06:11 -0400

    follow along some more

---
+++ Week 2/follow along 2/public/scripts/main.js
@@ -0,0 +1,9 @@
+function paint(color) {
+    console.log(color);
+    const circle = document.getElementById('circleID');
+    circle.style =  `background-color:${color}`
+    console.log(circle);
+}
+console.log('Script Loaded...');
+const circle = document.getElementById('circleID');
+circle.style = `background-color:${color}`;
