d7d2103255206203d704d8dafd43f668e82c257f
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2023-09-20 17:32:08 -0400

    hw5

---
+++ Week3/practice-5/main.js
@@ -0,0 +1,66 @@
+document.getElementById("demo").innerHTML = "Hello JavaScript"; 
+document.getElementById('demo').innerHTML = 'Hello JavaScript'; 
+document.getElementById("demo").style.fontSize = "35px"; 
+document.getElementById("demo").style.display = "none"; 
+document.getElementById("demo").style.display = "block"; 
+// <script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+document.getElementById("demo").innerHTML = 5 + 6;
+document.write(5 + 6);
+window.alert(5 + 6);
+alert(5 + 6);
+console.log(5 + 6);
+window.print();
+let x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+document.getElementById("demo").innerHTML = "Hello Dolly."; 
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+a = 5; b = 6; c = a + b;
+let person = "Hege";
+document.getElementById("demo").innerHTML =
+"Hello Dolly!"; 
+function myFunction2() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
+var s, t;
+s = 5 + 6;
+t = x * 10;
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+/*
+comment comment comment
+abc123
+*/
+const d = 3;
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
+let _lastName = "Johnson";
+let _x = 2;
+let _100 = 5;
+5 & 1;
+5 | 1;
+~ 5;
+5 ^ 1;
+5 << 1;
+5 >> 1;
+5 >>> 1;
+const date = new Date("2022-03-25"); 
+let v = true;
+let w = null;
+let q = undefined;
+let xx = BigInt("123456789012345678901234567890");
+typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4)        // Returns "number" 
