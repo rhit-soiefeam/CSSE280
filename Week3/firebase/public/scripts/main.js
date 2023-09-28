c9e8f75b113dcc5ddf4aa13fa6b10dd7177a53d5
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2023-09-28 10:33:55 -0400

    firebase exam

--- Week3/firebase/public/scripts/main.js
+++ Week3/firebase/public/scripts/main.js
@@ -2,10 +2,11 @@ window.onresize = screen;
 window.onload = screen;
 function screen() {
     Width = window.innerWidth;
-    document.querySelector("#size").innerHTML= `Width : ${Width} px`;
+    document.querySelector("#size").innerHTML= `幅広さ ： ${Width} ｐｘ`;
 }
 main = function() {
     console.log(`準備できた`);
     console.log(window.innerWidth);
     screen();
 }
+main();
