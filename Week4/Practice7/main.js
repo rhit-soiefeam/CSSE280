a2ccc42e434bd77b4b51ea77c96ba67cf072b9c0
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2023-09-29 16:09:19 -0400

    a

--- Week4/Practice7/main.js
+++ Week4/Practice7/main.js
@@ -10,7 +10,24 @@ for(let number of numbers) {
     sum += number;
 }
 console.log(sum);
+const student = {name: 'John', age: 20, grade: 'A'};
+for(let item in student) {
+    console.log(item);
+}
+console.log(sum);
 const book = {title: 'Harry Potter', author: 'J.K. Rowling', year: 1997};
 for(let item in book) {
     console.log(book[item]);
 }
+const numbers1= [1, 2, 3, 4, 5];
+let sum1 = 0;
+for(let number of numbers1) {
+    sum1 += number;
+}
+console.log(sum);
+
+const temperatures = [32, 68, 75, 82, 56];
+function farToCel(farTemp) {
+    console.log((farTemp - 32) * 5/9);
+};
+console.log(temperatures.forEach(farToCel));
