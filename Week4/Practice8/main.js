c8cb9ad772e91fcd39f240902ea8717180b49621
Alexander Soiefer <69055950+Alexander-Soiefer@users.noreply.github.com>
2023-09-29 16:46:38 -0400

    aaaaa

---
+++ Week4/Practice8/main.js
@@ -0,0 +1,27 @@
+function mult(a, b){
+    return a * b;   
+};
+function isEven(a) {
+    return (a % 2 === 0);
+};
+const difference1 = function subtract1 (a, b) 
+{
+    return b;
+};
+
+const greeting = function(name="Albert") {
+    console.log(`Hello ${name}`);
+};
+function calculate(a, b) {
+    return (a * a + b * b);
+}
+function average(a, b, c) {
+    return ((a + b + c) /  2);
+}
+multiply = (a, b) => a * b;
+greet = (a) => `Hello ${a}!`
+square = (x) => x * x;
+isOdd = (x) => x % 2 === 1 || x % 2 === -1;
+findMax = (...numbers) => Math.max(...numbers);
+addTwoNumbers = (a, b) => a + b;
+greeting();
